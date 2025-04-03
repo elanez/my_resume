3309ca4ce9b3d0e82793440f3be3933c77d12f4b
Elan Fernandez <fernandezelan@gmail.com>
2025-04-03 21:21:21 +0800

    Created post page and css
    - Created with gemini 2.0 pro and 3.7 sonnet

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.all_posts, name="all-posts"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
 
 def all_posts(request):
-    return render(request, 'blog/all-posts.html')
+    latest_posts = Post.objects.all().order_by("-date")
+    return render(request, 'blog/all-posts.html', {
+        "all_posts": latest_posts
+    })
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post-detail.html', {
+        "post": post,
+        "tags": post.tags.all()
+    })
