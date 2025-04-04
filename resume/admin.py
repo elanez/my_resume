bd96d8317c0b358092be292487b3298173b39d51
Elan Fernandez <fernandezelan@gmail.com>
2025-04-05 00:12:18 +0800

    Male slugs auto fill

--- resume/admin.py
+++ resume/admin.py
@@ -8,4 +8,10 @@ admin.site.register(PersonalInformation)
 admin.site.register(Skill)
 admin.site.register(Education)
 admin.site.register(Experience)
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'is_ongoing')
+    list_filter = ('is_ongoing', 'start_date')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Project, ProjectAdmin)
