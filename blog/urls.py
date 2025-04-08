d035a760e4949842e940ee651db26184ec127571
Elan Fernandez <fernandezelan@gmail.com>
2025-04-08 15:06:09 +0800

    Added remove from read later

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
     path("read-later/", views.read_later_view, name="read-later-page"),
     path("add-to-read-later/<slug:slug>", views.add_to_read_later, name="add-to-read-later"),
+    path("remove-from-read-later/<slug:slug>", views.remove_from_read_later, name="remove-from-read-later"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,9 +11,12 @@ def all_posts(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    read_later_list = request.session.get('read_later', [])
+    is_in_read_later = slug in read_later_list
     return render(request, 'blog/post-detail.html', {
         "post": post,
-        "tags": post.tags.all()
+        "tags": post.tags.all(),
+        "is_in_read_later": is_in_read_later
     })
 
 def read_later_view(request):
@@ -29,5 +32,13 @@ def add_to_read_later(request, slug):
         read_later_list = request.session.get('read_later', [])
         if slug not in read_later_list:
             read_later_list.append(slug)
+            request.session['read_later'] = read_later_list
+    return redirect('post-detail-page', slug=slug)
+
+def remove_from_read_later(request, slug):
+    if request.method == 'POST':
+        read_later_list = request.session.get('read_later', [])
+        if slug in read_later_list:
+            read_later_list.remove(slug)
             request.session['read_later'] = read_later_list
     return redirect('post-detail-page', slug=slug)
