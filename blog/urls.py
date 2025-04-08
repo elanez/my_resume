a914ce9b2c77a26b8ee17462d19ebac8c32174d7
Elan Fernandez <fernandezelan@gmail.com>
2025-04-08 15:00:59 +0800

    Added read-later feature

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.all_posts, name="all-posts"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
+    path("read-later/", views.read_later_view, name="read-later-page"),
+    path("add-to-read-later/<slug:slug>", views.add_to_read_later, name="add-to-read-later"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 # Create your views here.
@@ -15,3 +15,19 @@ def post_detail(request, slug):
         "post": post,
         "tags": post.tags.all()
     })
+
+def read_later_view(request):
+    read_later_list = request.session.get('read_later', [])
+    posts = Post.objects.filter(slug__in=read_later_list)
+    return render(request, 'blog/read-later.html', {
+        'posts': posts
+    })
+
+def add_to_read_later(request, slug):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, slug=slug)
+        read_later_list = request.session.get('read_later', [])
+        if slug not in read_later_list:
+            read_later_list.append(slug)
+            request.session['read_later'] = read_later_list
+    return redirect('post-detail-page', slug=slug)
